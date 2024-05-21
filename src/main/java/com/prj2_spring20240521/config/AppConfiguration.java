0e2c65dfe39773689cb9b16228a9272c7f88cf72
Dongwon <kdw686968@gmail.com>
2024-05-21 14:29:58 +0900

    게시물 목록 보기

--- src/main/java/com/prj2_spring20240521/config/AppConfiguration.java
+++ src/main/java/com/prj2_spring20240521/config/AppConfiguration.java
@@ -60,3 +60,4 @@ public class AppConfiguration {
         return http.build();
     }
 }
+
--- src/main/java/com/prj2_spring20240521/controller/board/BoardController.java
+++ src/main/java/com/prj2_spring20240521/controller/board/BoardController.java
@@ -5,10 +5,9 @@ import com.prj2_spring20240521.domain.board.Board;
 import com.prj2_spring20240521.service.board.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/board")
@@ -19,11 +18,17 @@ public class BoardController {
 
     @PostMapping("add")
     public ResponseEntity add(@RequestBody Board board) {
+//        Thread.sleep(10000); => 응답 지연 시키는 메소드인데 이거 말고도 몇개 더 필요함.  쓸 일 있을때 강의 영상05/21 참조
         if (service.validate(board)) {
             service.add(board);
             return ResponseEntity.ok().build();
         } else {
             return ResponseEntity.badRequest().build();
         }
+    }
+
+    @GetMapping("list")
+    public List<Board> list() {
+        return service.list();
     }
 }
