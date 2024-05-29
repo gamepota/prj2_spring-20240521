a1dfcfba766a7142da78dd73500275bfeafdc96c
Dongwon <kdw686968@gmail.com>
2024-05-29 10:25:03 +0900

    파일을 s3에 업로드

--- src/main/java/com/prj2_spring20240521/service/board/BoardService.java
+++ src/main/java/com/prj2_spring20240521/service/board/BoardService.java
@@ -5,11 +5,15 @@ import com.prj2_spring20240521.domain.board.BoardFile;
 import com.prj2_spring20240521.mapper.board.BoardMapper;
 import com.prj2_spring20240521.mapper.member.MemberMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.ObjectCannedACL;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.io.File;
 import java.io.IOException;
@@ -24,6 +28,8 @@ public class BoardService {
     private final BoardMapper mapper;
     private final MemberMapper memberMapper;
     final S3Client s3Client;
+    @Value("${aws.s3.bucket.name}")
+    String bucketName;
 
     public void add(Board board, MultipartFile[] files, Authentication authentication) throws IOException {
         board.setMemberId(Integer.valueOf(authentication.getName()));
@@ -36,16 +42,29 @@ public class BoardService {
                 mapper.insertFileName(board.getId(), file.getOriginalFilename());
                 // 실제 파일 저장
                 // 부모 디렉토기 만들기
-                String dir = STR."C:/Temp/prj2/\{board.getId()}";
-                File dirFile = new File(dir);
-                if (!dirFile.exists()) {
-                    dirFile.mkdirs();
-                }
+//                String dir = STR."C:/Temp/prj2/\{board.getId()}";
+//                File dirFile = new File(dir);
+//                if (!dirFile.exists()) {
+//                    dirFile.mkdirs();
+//                }
+//
+//                //파일경로
+//                String path = STR."C:/Temp/prj2/\{board.getId()}/\{file.getOriginalFilename()}";
+//                File destination = new File(path);
+//                file.transferTo(destination);
 
-                //파일경로
-                String path = STR."C:/Temp/prj2/\{board.getId()}/\{file.getOriginalFilename()}";
-                File destination = new File(path);
-                file.transferTo(destination);
+                // 실제 파일 저장(new)
+                String key = STR."prj2/\{board.getId()}/\{file.getOriginalFilename()}";
+                PutObjectRequest objectRequest = PutObjectRequest
+                        .builder()
+                        .bucket(bucketName)
+                        .key(key)
+                        .acl(ObjectCannedACL.PUBLIC_READ)
+                        .build();
+
+                s3Client.putObject(objectRequest,
+                        RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
+
             }
         }
 
